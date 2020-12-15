6288142f3e4822ee02a7226e87703ca71bdb1d39
Michael Gerzabek <michael.gerzabek@gmail.com>
2020-12-15 18:49:18 +0100

    0.1.15

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # jekyll-emd-generator
+
 Enhance Jekyll Frontmatter from JSON Files
+
+## dev
+
+> gem build jekyll-emd-generator.gemspec -V --debug
+
+> gem push jekyll-emd-generator-0.0.15.gem (Versionsnummer entsprechend anpassen!)
--- lib/jekyll-emd-generator.rb
+++ lib/jekyll-emd-generator.rb
@@ -14,44 +14,44 @@ module EnhanceMetaData
           end
         end
         if File.fnmatch( "**/index.{html,md}", page.path, File::FNM_EXTGLOB )
-          ["product", "elements"].each do |method|
+          ["offer", "elements"].each do |method|
             Generator.method(method).call(page,site)
           end
         end
       end
     end
 
-    def self.product(doc,site)
+    def self.offer(doc,site)
 
       dir = File.dirname(doc.path)
-      file = site.in_source_dir(dir,"product.json")
+      file = site.in_source_dir(dir,"offer.json")
       if File.exist?(file)
 
-        doc.data['product'] = SafeYAML.load_file(file)
+        doc.data['offer'] = SafeYAML.load_file(file)
         #
-        Jekyll.logger.info "", file
+        #Jekyll.logger.info "", file
         #
         # jekyll-seo-plugin @title
         # Der Product.name wird zu @title falls nicht im 
         # Frontmatter überschrieben
         if doc.data['title'].nil?
-          doc.data['title'] = doc.data['product']['name']
+          doc.data['title'] = doc.data['offer']['name']
         end
         # jekyll-seo-plugin @description
         # Product.description wird zu @description falls nicht
         # im Frontmatter überschrieben
         if doc.data['description'].nil?
-          doc.data['description'] = doc.data['product']['description']
+          doc.data['description'] = doc.data['offer']['description']
         end
         # jekyll-seo-plugin @image
         # Product.image wird zu @image falls nicht im
         # Frontmatter überschrieben
         if doc.data['image'].nil?
-          image = site.in_source_dir(dir,doc.data['product']['image'])
+          image = site.in_source_dir(dir,doc.data['offer']['image'])
           if File.exist?(image)
             dimensions = FastImage.size(image)
             doc.data['image'] = {}
-            doc.data['image']['path'] = '/' + dir + '/' + doc.data['product']['image']
+            doc.data['image']['path'] = '/' + dir + '/' + doc.data['offer']['image']
             doc.data['image']['width'] = dimensions[0]
             doc.data['image']['height'] = dimensions[1]
           else
@@ -67,7 +67,7 @@ module EnhanceMetaData
       file = site.in_source_dir(dir,"elements.json")
       if File.exist?(file)
         #
-        Jekyll.logger.info "", file
+        #Jekyll.logger.info "", file
         #
         doc.data['elements'] = SafeYAML.load_file(file)
       end
